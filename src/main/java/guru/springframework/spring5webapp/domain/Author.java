65466ff63fd27a6590899dcf2768786dbd0ccadf
Jorge B. Dutton <jorge.dutton@gmail.com>
2020-04-06 08:21:12 +0200

    Thymeleaf templates and circular ref resolution

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -65,8 +65,7 @@ public class Author {
 
 	@Override
 	public String toString() {
-		return "Author [id=" + id + ", firstName=" + firstName + ", lastName="
-				+ lastName + ", books=" + books + "]";
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
 	}
 
 	@Override
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -98,7 +98,7 @@ public class Publisher {
 	@Override
 	public String toString() {
 		return "Publisher [id=" + id + ", name=" + name + ", address=" + address + ", city=" + city
-				+ ", state=" + state + ", zip=" + zip + ", books=" + books + "]";
+				+ ", state=" + state + ", zip=" + zip + "]";
 	}
 
 	@Override
